20b67dc44343dd569fe0bba79ede920b18e4e12b
banalna <judas.priest999@gmail.com>
2020-12-08 17:45:28 +0300

    fix markup docs

--- persistence/IdentifiableMongoDbPersistence.go
+++ persistence/IdentifiableMongoDbPersistence.go
@@ -25,30 +25,30 @@ accessing c.Collection properties.
 
 Configuration parameters:
 
-- collection:                  (optional) MongoDB collection name
-- connection(s):
-  - discovery_key:             (optional) a key to retrieve the connection from IDiscovery
-  - host:                      host name or IP address
-  - port:                      port number (default: 27017)
-  - uri:                       resource URI or connection string with all parameters in it
-- credential(s):
-  - store_key:                 (optional) a key to retrieve the credentials from ICredentialStore
-  - username:                  (optional) user name
-  - password:                  (optional) user password
-- options:
-  - max_pool_size:             (optional) maximum connection pool size (default: 2)
-  - keep_alive:                (optional) enable connection keep alive (default: true)
-  - connect_timeout:           (optional) connection timeout in milliseconds (default: 5000)
-  - socket_timeout:            (optional) socket timeout in milliseconds (default: 360000)
-  - auto_reconnect:            (optional) enable auto reconnection (default: true) (not used)
-  - reconnect_interval:        (optional) reconnection interval in milliseconds (default: 1000) (not used)
-  - max_page_size:             (optional) maximum page size (default: 100)
-  - replica_set:               (optional) name of replica set
-  - ssl:                       (optional) enable SSL connection (default: false) (not implements in this release)
-  - auth_source:               (optional) authentication source
-  - auth_user:                 (optional) authentication user name
-  - auth_password:             (optional) authentication user password
-  - debug:                     (optional) enable debug output (default: false). (not used)
+  - collection:                  (optional) MongoDB collection name
+  - connection(s):
+    - discovery_key:             (optional) a key to retrieve the connection from IDiscovery
+    - host:                      host name or IP address
+    - port:                      port number (default: 27017)
+    - uri:                       resource URI or connection string with all parameters in it
+  - credential(s):
+    - store_key:                 (optional) a key to retrieve the credentials from ICredentialStore
+    - username:                  (optional) user name
+    - password:                  (optional) user password
+  - options:
+    - max_pool_size:             (optional) maximum connection pool size (default: 2)
+    - keep_alive:                (optional) enable connection keep alive (default: true)
+    - connect_timeout:           (optional) connection timeout in milliseconds (default: 5000)
+    - socket_timeout:            (optional) socket timeout in milliseconds (default: 360000)
+    - auto_reconnect:            (optional) enable auto reconnection (default: true) (not used)
+    - reconnect_interval:        (optional) reconnection interval in milliseconds (default: 1000) (not used)
+    - max_page_size:             (optional) maximum page size (default: 100)
+    - replica_set:               (optional) name of replica set
+    - ssl:                       (optional) enable SSL connection (default: false) (not implements in this release)
+    - auth_source:               (optional) authentication source
+    - auth_user:                 (optional) authentication user name
+    - auth_password:             (optional) authentication user password
+    - debug:                     (optional) enable debug output (default: false). (not used)
 
 References:
 
@@ -58,78 +58,77 @@ References:
 
 Example:
 
-	type MyMongoDbPersistence  struct {
-		IdentifiableMongoDbPersistence
+  type MyMongoDbPersistence  struct {
+    IdentifiableMongoDbPersistence
+  }
+
+  func NewMyMongoDbPersistence() {
+    proto := reflect.TypeOf(MyData{})
+    return &DummyMongoDbPersistence{*mngpersist.NewIdentifiableMongoDbPersistence(proto, "mydata")}
+  }
+
+  composeFilter(filter cdata.FilterParams) interface{} {
+    if &filter == nil {
+      filter = *cdata.NewEmptyFilterParams()
 	}
 
-    func NewMyMongoDbPersistence() {
-		proto := reflect.TypeOf(MyData{})
-		return &DummyMongoDbPersistence{*mngpersist.NewIdentifiableMongoDbPersistence(proto, "mydata")}
-    }
-
-    composeFilter(filter cdata.FilterParams) interface{} {
-        if &filter == nil {
-			filter = *cdata.NewEmptyFilterParams()
-		}
-
-		name := filter.GetAsNullableString("name")
-		var filterObj bson.M
-		if *name != "" {
-			filterObj = bson.M{"name": *name}
-		 else {
-			filterObj = bson.M{}
-		}
-		return filterObj
-    }
-
-    func (c *MyMongoDbPersistence) GetPageByFilter(correlationId string, filter cdata.FilterParams, paging cdata.PagingParams) (page MyDataPage, err error){
-        tempPage, err := c.IdentifiableMongoDbPersistence.GetPageByFilter(correlationId,
-		composeFilter(filter), paging, nil, nil)
-		// Convert to MyDataPage
-		dataLen := int64(len(tempPage.Data)) // For full release tempPage and delete this by GC
-		data := make([]MyData, dataLen)
-		for i, v := range tempPage.Data {
-			data[i] = v.(MyData)
-		}
-		page = *NewMyDataPage(&dataLen, data)
-		return page, err
-    }
-
-    persistence = NewMyMongoDbPersistence()
-    persistence.Configure(NewConfigParamsFromTuples(
-        "host", "localhost",
-		"port", "27017"
-		"database", "test",
-    ))
-
-	opnErr := persitence.Open("123")
-	if opnErr != nil {
-		...
+    name := filter.GetAsNullableString("name")
+    var filterObj bson.M
+	if *name != "" {
+	    filterObj = bson.M{"name": *name}
+	  else {
+	    filterObj = bson.M{}
 	}
+	return filterObj
+  }
 
-	crtRes, crtErr := persistence.Create("123", MyData{ id: "1", name: "ABC" })
-	if crtErr != nil {
-		...
-	}
-	getRes, getErr := persistence.GetPageByFilter("123", NewFilterParamsFromTuples("name", "ABC"), nil)
-    if getErr != nil {
-		...
-	}
-	fmt.Println(getRes.Data);          // Result: { id: "1", name: "ABC" }
+  func (c *MyMongoDbPersistence) GetPageByFilter(correlationId string, filter cdata.FilterParams, paging cdata.PagingParams) (page MyDataPage, err error){
+      tempPage, err := c.IdentifiableMongoDbPersistence.GetPageByFilter(correlationId,
+  	  composeFilter(filter), paging, nil, nil)
+  	  // Convert to MyDataPage
+  	  dataLen := int64(len(tempPage.Data)) // For full release tempPage and delete this by GC
+  	  data := make([]MyData, dataLen)
+  	  for i, v := range tempPage.Data {
+  	    data[i] = v.(MyData)
+  	  }
+  	  page = *NewMyDataPage(&dataLen, data)
+  	  return page, err
+  }
 
-	persistence.deleteById("123", "1")
+  persistence = NewMyMongoDbPersistence()
+  persistence.Configure(NewConfigParamsFromTuples(
+    "host", "localhost",
+  	"port", "27017"
+  	"database", "test",
+  ))
+
+  opnErr := persitence.Open("123")
+  if opnErr != nil {
+  	...
+  }
+
+  crtRes, crtErr := persistence.Create("123", MyData{ id: "1", name: "ABC" })
+  if crtErr != nil {
+	...
+  }
+  getRes, getErr := persistence.GetPageByFilter("123", NewFilterParamsFromTuples("name", "ABC"), nil)
+  if getErr != nil {
+	...
+  }
+  fmt.Println(getRes.Data);          // Result: { id: "1", name: "ABC" }
+
+  persistence.deleteById("123", "1")
 	...
 */
-
 type IdentifiableMongoDbPersistence struct {
 	MongoDbPersistence
 }
 
 // NewIdentifiableMongoDbPersistence is creates a new instance of the persistence component.
 // Parameters:
-// 	- proto reflect.Type
-// 	type of saved data, need for correct decode from DB
-// 	- collection string
+//  - proto reflect.Type
+//  type of saved data, need for correct decode from DB
+//  - collection string
 //  (optional) a collection name.
 // Return *IdentifiableMongoDbPersistence
 // new created IdentifiableMongoDbPersistence component
@@ -146,7 +145,7 @@ func NewIdentifiableMongoDbPersistence(proto reflect.Type, collection string) *I
 
 // Configure is configures component by passing configuration parameters.
 // Parameters:
-// 	- config  *cconf.ConfigParams
+//  - config  *cconf.ConfigParams
 //  configuration parameters to be set.
 func (c *IdentifiableMongoDbPersistence) Configure(config *cconf.ConfigParams) {
 	c.MongoDbPersistence.Configure(config)
@@ -155,10 +154,10 @@ func (c *IdentifiableMongoDbPersistence) Configure(config *cconf.ConfigParams) {
 
 // GetListByIds is gets a list of data items retrieved by given unique ids.
 // Parameters:
-// 	- correlationId  string
-//	(optional) transaction id to Trace execution through call chain.
-//  - ids  []interface{}
-//	ids of data items to be retrieved
+//   - correlationId  string
+//   (optional) transaction id to Trace execution through call chain.
+//   - ids  []interface{}
+//   ids of data items to be retrieved
 // Returns items []interface{}, err error
 // a data list and error, if theq are occured.
 func (c *IdentifiableMongoDbPersistence) GetListByIds(correlationId string, ids []interface{}) (items []interface{}, err error) {
@@ -171,9 +170,9 @@ func (c *IdentifiableMongoDbPersistence) GetListByIds(correlationId string, ids 
 
 // GetOneById is gets a data item by its unique id.
 // Parameters:
-// - correlationId     (optional) transaction id to Trace execution through call chain.
-// - id                an id of data item to be retrieved.
-// - callback          callback function that receives data item or error.
+//   - correlationId     (optional) transaction id to Trace execution through call chain.
+//   - id                an id of data item to be retrieved.
+//   - callback          callback function that receives data item or error.
 func (c *IdentifiableMongoDbPersistence) GetOneById(correlationId string, id interface{}) (item interface{}, err error) {
 	filter := bson.M{"_id": id}
 	docPointer := c.NewObjectByPrototype()
@@ -193,9 +192,9 @@ func (c *IdentifiableMongoDbPersistence) GetOneById(correlationId string, id int
 
 // Create was creates a data item.
 // Parameters:
-// 	- correlation_id string
-//	(optional) transaction id to Trace execution through call chain.
-// 	- item interface{}
+//   - correlation_id string
+//   (optional) transaction id to Trace execution through call chain.
+//   - item interface{}
 // an item to be created.
 // Returns result interface{}, err error
 // created item and error, if they are occured
@@ -222,10 +221,10 @@ func (c *IdentifiableMongoDbPersistence) Create(correlationId string, item inter
 // Set is sets a data item. If the data item exists it updates it,
 // otherwise it create a new data item.
 // Parameters:
-// 	- correlation_id string
-//	(optional) transaction id to Trace execution through call chain.
-// 	- item interface{}
-//	 a item to be set.
+//   - correlation_id string
+//   (optional) transaction id to Trace execution through call chain.
+//   - item interface{}
+//   a item to be set.
 // Returns result interface{}, err error
 // updated item and error, if they occured
 func (c *IdentifiableMongoDbPersistence) Set(correlationId string, item interface{}) (result interface{}, err error) {
@@ -264,10 +263,10 @@ func (c *IdentifiableMongoDbPersistence) Set(correlationId string, item interfac
 
 // Update is updates a data item.
 // Parameters:
-// 	- correlation_id string
-//	(optional) transaction id to Trace execution through call chain.
-// 	- item  interface{}
-//  an item to be updated.
+//   - correlation_id string
+//   (optional) transaction id to Trace execution through call chain.
+//   - item  interface{}
+//   an item to be updated.
 // Returns result interface{}, err error
 // updated item and error, if theq are occured
 func (c *IdentifiableMongoDbPersistence) Update(correlationId string, item interface{}) (result interface{}, err error) {
@@ -302,12 +301,12 @@ func (c *IdentifiableMongoDbPersistence) Update(correlationId string, item inter
 
 // UpdatePartially is updates only few selected fields in a data item.
 // Parameters:
-// 	- correlation_id string
-// 	(optional) transaction id to Trace execution through call chain.
-// 	- id interface{}
-// 	an id of data item to be updated.
-// 	- data  cdata.AnyValueMap
-//	a map with fields to be updated.
+//   - correlation_id string
+//   (optional) transaction id to Trace execution through call chain.
+//   - id interface{}
+//   an id of data item to be updated.
+//   - data  cdata.AnyValueMap
+//   a map with fields to be updated.
 // Returns item interface{}, err error
 // updated item and error, if they are occured
 func (c *IdentifiableMongoDbPersistence) UpdatePartially(correlationId string, id interface{}, data *cdata.AnyValueMap) (item interface{}, err error) {
@@ -343,10 +342,10 @@ func (c *IdentifiableMongoDbPersistence) UpdatePartially(correlationId string, i
 
 // DeleteById is deleted a data item by it"s unique id.
 // Parameters:
-// 	- correlation_id string
-//  (optional) transaction id to Trace execution through call chain.
-// 	- id  interface{}
-//  an id of the item to be deleted
+//   - correlation_id string
+//   (optional) transaction id to Trace execution through call chain.
+//   - id  interface{}
+//   an id of the item to be deleted
 // Returns item interface{}, err error
 // deleted item and error, if they are occured
 func (c *IdentifiableMongoDbPersistence) DeleteById(correlationId string, id interface{}) (item interface{}, err error) {
@@ -370,10 +369,10 @@ func (c *IdentifiableMongoDbPersistence) DeleteById(correlationId string, id int
 }
 
 // DeleteByIds is deletes multiple data items by their unique ids.
-// - correlationId string
-// (optional) transaction id to Trace execution through call chain.
-// - ids  []interface{}
-// ids of data items to be deleted.
+//   - correlationId string
+//   (optional) transaction id to Trace execution through call chain.
+//   - ids  []interface{}
+//   ids of data items to be deleted.
 // Retrun error
 // error or nil for success.
 func (c *IdentifiableMongoDbPersistence) DeleteByIds(correlationId string, ids []interface{}) error {
